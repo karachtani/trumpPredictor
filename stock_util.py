c64f00c289c411be14ad0dd7315d73aa1fc4d386
Colby Tobin <>
2019-11-11 16:35:15 -0500

    Updated to retrieve open and close information as well as return positive or negative stock change

--- stock_util.py
+++ stock_util.py
@@ -1,15 +1,18 @@
 import pandas as pd
 pd.core.common.is_list_like = pd.api.types.is_list_like
 from alpha_vantage.timeseries import TimeSeries
+import numpy as np
 
 def get_single_stock_data(ticker = 'SPY', start_date = '2017-01-05', end_date = '2019-01-05'):
     ts = TimeSeries(key='L906TTW2PFZCXCVW', output_format='pandas')
     # Get json object with the intraday data and another with  the call's metadata
     data, meta_data = ts.get_daily_adjusted(ticker, outputsize='full')
+    # print(meta_data)
+    # print(data)
     data = data[data.index >= start_date]
     data = data[data.index <= end_date]
-    data = data['5. adjusted close'].rename(ticker)
-    print(data)
+    data = pd.concat([data['1. open'], data['4. close'], data['5. adjusted close']], axis=1)
+    # print(data)
     return data
 
 def get_multi_stock_data(tickers = [], start_date = '2017-01-05', end_date = '2019-01-05'):
@@ -23,3 +26,22 @@ def get_multi_stock_data(tickers = [], start_date = '2017-01-05', end_date = '20
     data = data.fillna(method='ffill').fillna(method='bfill')
 
     return data
+
+def clean_stock_data(data):
+
+    # data['Price Change'] = 1 if data['1. open'] < data['5. adjusted close'] else -1
+
+    data['Default Price Change'] = np.where((data['1. open'] <= data['4. close'])
+             , 1, -1)
+    data['Adjusted Price Change'] = np.where((data['1. open'] <= data['5. adjusted close'])
+                                            , 1, -1)
+
+    # print(data)
+    return data
+
+
+
+data = get_single_stock_data()
+cleaned_data = clean_stock_data(data)
+print(cleaned_data)
+
